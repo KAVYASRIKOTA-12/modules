0e9d6d139d72e88fd2725304eb870853d5213d3e
kavyasri kota <Kavyasrikota989@gmail.com>
2026-06-24 14:21:40 +0530

    Add files via upload

---
+++ modules.py
@@ -0,0 +1,66 @@
+#math module
+'''import math
+print(math.pi)
+print(math.pi*3)
+print(math.sqrt(2))
+print(math.pow(2,2))
+print(math.log(10))
+print(math.sin(30))
+print(math.cos(30))
+print(math.tan(45))
+print(math.ceil(6.9))
+print(math.ceil(0.5))
+print(math.floor(4.9))
+print(math.factorial(12))'''
+
+'''from math import pi,sqrt,log
+print(pi)
+print(sqrt(4))
+print(log(20))'''
+
+
+#sys module
+'''import sys
+print(sys.path)'''
+
+
+'''import sys
+for i in sys.path:  #to print in different lines
+    print(i)'''
+
+'''import sys
+print(sys.version)'''
+
+
+#os module
+'''import os
+print(os.path)
+print(os.getcwd())
+#print(os.listdir())
+#print(os.mkdir("may25"))
+print(os.chdir("C:\\Users\\Kavya\\Downloads"))
+print(os.listdir())'''
+
+
+#random module->To generate the random number in python , randinit function is used this function is defined in random module.Python defines a set of functions that are used to generate or manipulate random numbers through the module.
+'''import random
+a=random.sample(range(10,40),5)
+print(a)'''
+#randint()
+'''import random
+a=random.randint(20,50) #to print single number
+print(a)'''
+#choice()
+'''import random
+a=[10,20,30,40,50]
+b=random.choice(a)  #to print assigned numbers
+print(b)'''
+
+
+#task
+'''import random
+a=int(input("enter the roll of dice"))
+b=random.randint(1,7)'''
+
+
+
