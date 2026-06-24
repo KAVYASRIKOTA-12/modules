6c0b3231d0dd242b6765a1480fe0788a8e622af0
kavyasri kota <Kavyasrikota989@gmail.com>
2026-06-24 15:22:13 +0530

    Add files via upload

---
+++ mymodule.py
@@ -0,0 +1,26 @@
+#modules
+'''def greet(name):
+    print("Welcome",name)'''
+
+
+'''a=int(input("a value"))
+b=int(input("b value"))
+print("The sum is",a+b)'''
+
+'''details={"Idno":[10,20,30],
+         "Names":["kavya","harika","jhansi"],
+         "Marks":[90,80,70]}'''
+
+
+
+'''if __name__=="__main__":
+    a=[10,20,30,40,50]
+    a.extend("code")
+    print(a)'''
+
+'''def dummy():
+    if __name__=="__main__":
+        print("this program is run as script")
+    else:
+        print("this program is run as module")
+dummy()'''       
