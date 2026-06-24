5511c23017574e0a9e74993a0f0aed22a8ece8bd
kavyasri kota <Kavyasrikota989@gmail.com>
2026-06-24 15:23:02 +0530

    Add files via upload

---
+++ data.py
@@ -0,0 +1,20 @@
+'''import mymodule
+mymodule.greet("kavya")'''
+
+
+#import mymodule
+
+'''import mymodule           #error
+a=mymodule.details["Idno","Names"]
+print(a) '''                
+
+'''import mymodule
+a=mymodule.details["Idno"]
+b=mymodule.details["Names"]
+c=mymodule.details["Marks"]
+print(a)
+print(b)
+print(c)'''
+
+#import mymodule
+
